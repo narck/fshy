e40615194eb835b2d26a3f863300ea0acb3f5732
Nicolas Arkkila <nicolas@arkkila.fi>
2018-01-28 15:04:33 +0200

    2.8

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 const Person = ({p}) => {
   return (
-    <p>{p.name}</p>
+    <p>{p.name}: {p.number}</p>
   )
 }
 class App extends React.Component {
@@ -13,9 +13,10 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '1234' }
       ],
-      newName: ''
+      newName: '',
+      newNumber: ''
     }
   }
 
@@ -23,8 +24,10 @@ class App extends React.Component {
     const personExists = this.state.persons.map(x => x.name).includes(this.state.newName);
 
     e.preventDefault()
-    const newp = this.state.newName;
-    const persons = this.state.persons.concat({name: newp});
+    const name = this.state.newName;
+    const number = this.state.newNumber;
+    const persons = this.state.persons.concat({name, number});
+
     if (personExists) {
       alert('already exists')
     }
@@ -35,10 +38,8 @@ class App extends React.Component {
   }
 
 
-  handleNoteChange = (e) => {
-    this.setState({...this.state, newName: e.target.value})
-  }
-
+  handleNameChange = (e) => this.setState({...this.state, newName: e.target.value})
+  handleNumberChange = (e) => this.setState({...this.state, newNumber: e.target.value})
 
 
   render() {
@@ -50,7 +51,11 @@ class App extends React.Component {
         <form onSubmit={this.addPerson}>
           <div>
             nimi: <input             value={this.state.newNote}
-            onChange={this.handleNoteChange}/>
+            onChange={this.handleNameChange}/>
+          </div>
+          <div>
+            numero: <input             value={this.state.newNumber}
+            onChange={this.handleNumberChange}/>
           </div>
           <div>
             <button type="submit">lisää</button>
