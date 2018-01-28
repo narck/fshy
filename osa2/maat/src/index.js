c5d9a3d09a7147934d2431e17d44b0a493ca018b
Nicolas Arkkila <nicolas@arkkila.fi>
2018-01-28 15:48:00 +0200

    Nice

---
+++ osa2/maat/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+
+const App = () => {
+  return (
+    <div>
+<form>
+
+</form>
+
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
