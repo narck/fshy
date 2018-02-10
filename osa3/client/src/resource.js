426f2d740bda51e00433f61b26b63599d52c939a
Nicolas Arkkila <nicolas@arkkila.fi>
2018-02-10 19:56:45 +0200

    Modify resource to point to correct API

--- osa3/client/src/resource.js
+++ osa3/client/src/resource.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const add = (person) => axios.post("http://localhost:3001/api/persons", person)
-export const remove = (person) => axios.delete("http://localhost:3001/api/persons/" + person.id)
-export const update = (person) => axios.put("http://localhost:3001/api/persons/" + person.id, person)
-export const all = () => axios.get("http://localhost:3001/api/persons").then((r) => r.data)
+export const add = (person) => axios.post("http://huono.website/moro/api/persons", person)
+export const remove = (person) => axios.delete("http://huono.website/moro/api/persons/" + person.id)
+export const update = (person) => axios.put("http://huono.website/moro/api/persons/" + person.id, person)
+export const all = () => axios.get("http://huono.website/moro/api/persons").then((r) => r.data)
